2cc97a5d9caa94968e255ce6a6fcbe6cd68e8d81
Teltoriy <125376553+Teltoriy@users.noreply.github.com>
2023-02-26 22:06:33 +0300

    Update Figure.cpp

--- practice2/practice2/Figure.cpp
+++ practice2/practice2/Figure.cpp
@@ -32,40 +32,37 @@ void Figure::show()
 
 bool Figure::is_prug()
 {
-	float d13 = sqrt(pow(x3 - x1, 2) + pow(y3 - y1, 2)); // диагональ точек 1 и 3
-	float d24 = sqrt(pow(x4 - x2, 2) + pow(y4 - y2, 2)); // диагональ точек 2 и 4
+	float d13 = sqrt(pow(x3 - x1, 2) + pow(y3 - y1, 2)); 
+	float d24 = sqrt(pow(x4 - x2, 2) + pow(y4 - y2, 2)); 
 
-	// теперь будем проверять треугольники 4хугольника на соответствие критерию Пифагора
+	
 	if ((a * a + b * b) != d13 * d13) return false;
 	if ((b * b + c * c) != d24 * d24) return false;
 	if ((c * c + d * d) != d13 * d13) return false;
-	// если три угла 90, четвёртый 360-270 = 90
+	
 	return true;
 }
 
 bool Figure::is_square()
 {
-	// прямоугольник, у которого две смежные стороны равны
+	
 	return is_prug() && (a == b);
 }
 
 bool Figure::is_romb()
 {
-	// 4 стороны равны
+	
 	return (a == b) && (a == c) && (a == d);
 }
 
 bool Figure::is_in_circle()
 {
-	// найдём векторы 21 и 23 и косинус угла между ними
 	vec2 vec21 = { x1 - x2, y1 - y2 };
 	vec2 vec23 = { x3 - x2, y3 - y2 };
 	float cos123 = (vec21.x * vec23.x + vec21.y * vec23.y) / (a * b);
-	// найдём векторы 43 и 41 и косинус угла между ними
 	vec2 vec43 = { x3 - x4, y3 - y4 };
 	vec2 vec41 = { x1 - x4, y1 - y4 };
 	float cos345 = (vec43.x * vec41.x + vec43.y * vec41.y) / (c * d);
-	// т.к. сумма углов 180, то косинусы должны быть противоположны
 	return cos123 = -cos345;
 }
 
